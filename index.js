47ab1faf76965133d241aee78a51d4963de293cc
aniecres <111096788+aniecres@users.noreply.github.com>
2022-12-21 19:35:04 -0500

    start index.js code

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// Make Staff known
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Enigineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager')
+
+// HTML
+const generateHTML = 
